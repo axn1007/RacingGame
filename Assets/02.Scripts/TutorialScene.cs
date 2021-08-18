fe46c956906a50f8e1bc72291209fcb7706ab434
axn1007 <axn1007@naver.com>
2021-08-18 21:35:23 +0900

    TutorialScene

---
+++ Assets/02.Scripts/TutorialScene.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialScene : MonoBehaviour
+{
+    public GameObject methodCanvas;
+    public GameObject practiceCanvas;
+
+    public GameObject optionImage;
+    public GameObject optionImage1;
+    bool isOption = false;
+    bool isOption1 = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPracticeBtn()
+    {
+        methodCanvas.gameObject.SetActive(false);
+        practiceCanvas.gameObject.SetActive(true);
+
+    }
+
+    public void OnClickGoBtn()
+    {
+        SceneManager.LoadScene("KartSelect Scene");
+    }
+    
+    public void OnClickHomeBtn()
+    {
+        SceneManager.LoadScene("Home Scene");
+    }
+
+    public void OnClcikOptionbtn()
+    {
+        if (!isOption)
+        {
+            optionImage.gameObject.SetActive(true);
+            isOption = true;
+        }
+        else
+        {
+            optionImage.gameObject.SetActive(false);
+            isOption = false;
+        }
+    }
+    
+    public void OnClcikOptionbtn1()
+    {
+        if (!isOption1)
+        {
+            optionImage1.gameObject.SetActive(true);
+            isOption1 = true;
+        }
+        else
+        {
+            optionImage1.gameObject.SetActive(false);
+            isOption1 = false;
+        }
+    }
+
+    public void OnClickExitBtn()
+    {
+        Application.Quit();
+    }
+}
