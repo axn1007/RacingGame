020e60aaf147cd681439ea47c8fb34c35e130771
axn1007 <axn1007@naver.com>
2021-08-30 12:23:04 +0900

    Rank

--- Assets/02.Scripts/EndingSceneManager.cs
+++ Assets/02.Scripts/EndingSceneManager.cs
@@ -2,33 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using System.Text.RegularExpressions;
 
 public class EndingSceneManager : MonoBehaviour
 {
-    public TMP_InputField UserNameInput;
-
-    public TextMeshProUGUI rank;
-    public TextMeshProUGUI name;
+    public TextMeshProUGUI UserNameInput;
     public TextMeshProUGUI time;
+    public TMP_InputField userInput;
+    public Button enterBtn;
+
+    // À¯Àú Á¤º¸
+    public TextMeshProUGUI rankingRank;
+    public TextMeshProUGUI rankingName;
+    public TextMeshProUGUI rankingTime;
+    
+    // ScrollView - Content
+    public Transform content;
+    // ·©Å· Á¤º¸
     public GameObject ranking;
 
     private void Awake()
     {
         time.text = DataManager.nowPlayer.time;
+
+        //rankingRank = ranking.transform.Find("Rank");
+    }
+
+    private void FixedUpdate()
+    {
+        if (UserNameInput.text.Length > 1)
+        {
+            enterBtn.interactable = true;
+        }
+        else if (UserNameInput.text.Length > 10)
+        {
+            enterBtn.interactable = false;
+        }
     }
 
     public void OnClickRanking()
     {
-        DataManager.nowPlayer.UserNameArr = name.text;
-        //ranking.transform.GetChild.name = DataManager.nowPlayer.UserNameArr;
+        DataManager.nowPlayer.UserNameArr = UserNameInput.text;
+        rankingName.text = DataManager.nowPlayer.UserNameArr;
+        rankingTime.text = time.text;
 
-        //name = Regex.Replace(name, @"[^0-9a-zA-Z°¡-ÆR]");
+        GameObject R = Instantiate(ranking);
+        R.transform.parent = content;
+        R.gameObject.SetActive(true);
 
-        for (int i = 0; 10 > DataManager.nowPlayer.rankArr.Length; i++)
-        {
-            UserNameInput.text = DataManager.nowPlayer.UserNameArr;
-            time.text = DataManager.nowPlayer.rankArr[i];
-        }
+        userInput.gameObject.SetActive(false);
+        enterBtn.gameObject.SetActive(false);
     }
 }
